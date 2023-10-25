09aa4bb66afd51628b46b1aaa43121cf9f73af4b
mantisdev20 <aloroduns@yahoo.com>
2023-10-25 00:31:03 -0400

    create login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React, {useReducer, useState} from 'react';
 import './App.css';
 
+const formReducer = (state, event) => {
+  return {
+    ...state,
+    [event.name]: event.value
+  }
+}
+
 function App() {
+  const [formData, setFormData] = useReducer(formReducer, {});
+  const [submitting, setSubmitting] = useState(false);
+  
+  const handleSubmit = event => {
+    event.preventDefault();
+    setSubmitting(true);
+
+    setTimeout(() => {
+      setSubmitting(false);
+    }, 3000)
+  }
+
+  const handleChange = event => {
+    setFormData({
+      name: event.target.name,
+      value:event.target.value,
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>
+        Subscriber Form
+      </h1>
+      {submitting &&
+       <div>Submitting Form...</div>}
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <label>
+          <p>First Name</p>
+            <input name="firstName" onChange={handleChange} /> {/* Use name="firstName" */}
+            <p>Last Name</p>
+            <input name="lastName" onChange={handleChange}/> {/* Use name="lastName" */}
+          </label>
+        </fieldset>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
