fd1dc64300af3d0d572a983d7abc9fc9d94d6823
mantisdev20 <aloroduns@yahoo.com>
2023-10-26 00:42:15 -0400

    email and subscription updated

--- src/App.js
+++ src/App.js
@@ -1,46 +1,81 @@
-import React, {useReducer, useState} from 'react';
+import React, { useReducer, useState } from 'react';
 import './App.css';
 
 const formReducer = (state, event) => {
   return {
     ...state,
-    [event.name]: event.value
-  }
-}
+    [event.name]: event.value,
+  };
+};
 
 function App() {
   const [formData, setFormData] = useReducer(formReducer, {});
   const [submitting, setSubmitting] = useState(false);
-  
-  const handleSubmit = event => {
+
+  const handleSubmit = (event) => {
     event.preventDefault();
     setSubmitting(true);
 
     setTimeout(() => {
       setSubmitting(false);
-    }, 3000)
-  }
+    }, 3000);
+  };
 
-  const handleChange = event => {
+  const handleChange = (event) => {
     setFormData({
       name: event.target.name,
-      value:event.target.value,
+      value: event.target.value,
     });
-  }
+  };
+
   return (
     <div className="wrapper">
-      <h1>
-        Subscriber Form
-      </h1>
-      {submitting &&
-       <div>Submitting Form...</div>}
+      <h1>Subscriber Form</h1>
+      {submitting && <div>Submitting Form...</div>}
       <form onSubmit={handleSubmit}>
         <fieldset>
           <label>
-          <p>First Name</p>
-            <input name="firstName" onChange={handleChange} /> {/* Use name="firstName" */}
+            <p>First Name</p>
+            <input
+              name="firstName"
+              value={formData.firstName || ''}
+              onChange={handleChange}
+            />
             <p>Last Name</p>
-            <input name="lastName" onChange={handleChange}/> {/* Use name="lastName" */}
+            <input
+              name="lastName"
+              value={formData.lastName || ''}
+              onChange={handleChange}
+            />
+            {/* Input field for the user's email using <input> with type="email." The field is named "email," and the value is controlled by the formData.email state. */}
+            <p>Email</p>
+            <input 
+              name="email"
+              type="email"
+              value={formData.email || ''}
+              onChange={handleChange}
+            />
+            <label className="checkbox-label"> {/* Add a class name for styling */}
+            <p>Subscribe to Newsletter</p>
+            <input
+              name="subscribe"
+              type="checkbox"
+              checked={formData.subscribe || false}
+              onChange={handleChange}
+            />
+            </label>
+            {/*  a checkbox for subscribing to the newsletter, named "subscribe." The checkbox's 
+            value is controlled by the formData.subscribe state. */}
+            <p>Comment</p>
+            <textarea
+              name="comment"
+              value={formData.comment || ''}
+              onChange={handleChange}
+              rows="4"
+              cols="50"
+              placeholder="Write your comment here."
+            />
+          
           </label>
         </fieldset>
         <button type="submit">Submit</button>
@@ -50,3 +85,4 @@ function App() {
 }
 
 export default App;
+
